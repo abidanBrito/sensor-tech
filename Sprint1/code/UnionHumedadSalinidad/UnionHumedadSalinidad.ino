95f48edd86f4e477b127f5ab43d9847c9df869a1
Abidán Brito <43681148+abidanBrito@users.noreply.github.com>
2019-10-26 13:30:44 +0200

    Salinity & Humidity Integration program - v2.0
    Program integration complete. New header, comments and more structured coding standards. We need to run a few more tests, as we may have to change some macros (calibration bounds).

--- Sprint1/code/UnionHumedadSalinidad/UnionHumedadSalinidad.ino
+++ Sprint1/code/UnionHumedadSalinidad/UnionHumedadSalinidad.ino
@@ -1,123 +1,131 @@
-//-----------------------------------------------------------------------
-// Fecha: 21/10/2019
-// Autores/as: Pablo Enguix, Abidán Brito, Elvira Montagud y Luis Belloch
-//-----------------------------------------------------------------------
+/*  ------------------------------------------------------------------------
+    AUTHOR:        Abidan Brito, Elvira Montagud, Luis Belloch, Pablo Enguix
+    FILE:          UnionHumedadSalinidad.ino
+    DATE:          25/10/2019
+    STATE:         DONE
+    FUNCTIONALITY: Describe what this file is used for.
+    ------------------------------------------------------------------------
+    NOTICE: (C) Copyright 2019. All rights reserved.
+    ------------------------------------------------------------------------ */
 
+//// IMPORT EXTERNAL LIBRARIES ////
 #include <Wire.h>
+#include <Adafruit_ADS1015.h>       // ADS library
 
-//
-// Librería ADS
-//
-#include <Adafruit_ADS1015.h>
+//// MACRO DEFINITIONS ////
+#define POWER_PIN                   5           // Digital I/O
+#define INPUT_PIN_SALINITY          0           // Input salinity pin
+#define INPUT_PIN_HUMIDITY          1           // Input humidity pin
+#define SALINITY_NUM_READINGS       10          // Number of salinity readings
+#define HUMIDITY_NUM_READINGS       10          // Number of salinity readings
+#define SALINITY_RANGE_LOW          1450        // Salinity threshold in water (lower bound)
+#define SALINITY_RANGE_HIGH         6250        // Salinity threshold in water (upper bound)
+#define HUMIDITY_RANGE_LOW          1700        // Humidity threshold -> Dry value (lower bound)
+#define HUMIDITY_RANGE_HIGH         400         // Humidity threshold -> Soaked value (upper bound)
+#define DEEP_SLEEP_TIME             60000000    // Hibernating time
 
-//
-// se define el ads1115 en la dirección 0x48
-//
-Adafruit_ADS1115 ads1115(0x48);
+// Create an ADC object
+Adafruit_ADS1115 ads1115(0x48);     // Defined at address '0x48'
 
-//
-// Medimos valor en seco ~0%
-//
-const int aire = 1700;
-
-//
-// Medimos en agua ~100%
-//
-const int agua = 400;
-
+//----------------------------------------------------------------------
+// Initial configuration
+//----------------------------------------------------------------------
 void setup() {
+    // Set data rate for serial data transmission
+    Serial.begin(9600);
 
-  Serial.begin(9600);
+    // Turn on ADS1115
+    ads1115.begin();
 
-  //
-  // Encendemos el ads1115
-  //
-  ads1115.begin();                                                               //Inicializar el ADS
+    // Gain (ADC Range) -> +/- 4.096 V (1 bit = 2 mV)
+    ads1115.setGain(GAIN_ONE);
+} // setup()
 
-  //
-  // Situamos el rango del ADC: +/- 4.096V (1 bit=2mV)
-  //
-  ads1115.setGain(GAIN_ONE);
-
-} // ()
-
-//-----------------------------------------------------------------------
-// MedirHumedad -> R
-//-----------------------------------------------------------------------
-int MedirHumedad(){
-
-  int16_t datos[10];
-  int16_t adc0;
-  float humedad;
-  int sumatorio;
-
-
-  //
-  // Calcula la media de 10 datos
-  //
-  for(int i=0; i < 10; i++){
-
-  adc0 = ads1115.readADC_SingleEnded(0);
-
-  datos[i] = 100*aire/(aire-agua)-adc0*100/(aire-agua);
-
-  sumatorio += datos[i];
-
-  delay(50);
-
-  } // for
-
-  humedad = sumatorio / 10;
-
-  return(humedad);
-
-} // ()
-
-//-----------------------------------------------------------------------
-// MedirSalinidad -> R
-//-----------------------------------------------------------------------
-int MedirSalinidad(){
-
-  float salinidad;
-
-  return(salinidad);
-
-} // ()
-
-//-----------------------------------------------------------------------
-//-----------------------------------------------------------------------
-void MostrarDatos(float humedad, float salinidad){
-
-  //
-  // Mostramos la medida de humedad
-  //
-  Serial.print("HR (%): ");
-  Serial.print(humedad);
-  Serial.println("%");
-
-  //
-  // Mostramos la medida de salinidad
-  //
-  Serial.print("SAL (%): ");
-  Serial.print(salinidad);
-  Serial.println("%");
-
-}
-
-
-
+//----------------------------------------------------------------------
+// Runtime execution
+//----------------------------------------------------------------------
 void loop() {
+    // Salinity measure
+    float meanSalinity = readSalinityV2(SALINITY_NUM_READINGS);
+    printSensorReading(meanSalinity, "Salinity");
 
-  float humedad;
-  float salinidad;
-  const int segundosDormido = 60;
+    // Humidity measure
+    float meanHumidity = readHumidity(HUMIDITY_NUM_READINGS);
+    printSensorReading(meanHumidity, "Humidity");
 
-  humedad = MedirHumedad();
+    // Hibernate for a minute
+    //ESP.deepSleep(DEEP_SLEEP_TIME);
 
-  salinidad = MedirSalinidad();
+    // Hold on for a minute
+    delay(1000);
+} // loop()
 
-  MostrarDatos(humedad, salinidad);
+//----------------------------------------------------------------------
+// Several salinity readings. Return the average
+//----------------------------------------------------------------------
+float readSalinityV2(int numReadings) {
+    //  Float for precision and prevent overflow
+    float averageReading = 0.0, salinityPercentage = 0.0;
+    int sum = 0;
 
-  ESP.deepSleep(segundosDormido * 1000000);
+    for (int i = 0; i < numReadings; i++) {
+        digitalWrite(POWER_PIN, HIGH);                           //  Supply power to the sensor
+        delay(100);                                              //  Wait for sensor to settle
 
-} // ()
+        // Returns '16-bit int' (using ' 32-bit int' to prevent overflow)
+        sum += ads1115.readADC_SingleEnded(INPUT_PIN_SALINITY);  //  Add reading to the running sum
+        digitalWrite(POWER_PIN, LOW);                            //  Turn off power to the sensor
+        delay(10);                                               //  Wait between readings
+    } // for
+
+    // Get average reading value
+    averageReading = sum / numReadings;
+
+    // Get percentage
+    salinityPercentage = map(averageReading, SALINITY_RANGE_LOW,
+                                    SALINITY_RANGE_HIGH, 0, 100);
+
+    return salinityPercentage;
+} // readSalinityV2()
+
+//-----------------------------------------------------------------------
+// Several humidity readings. Return the average
+//-----------------------------------------------------------------------
+int readHumidity(int numReadings) {
+    float averageReading = 0.0, humidityPercentage = 0.0;
+    int sum = 0;
+
+    for(int i = 0; i < numReadings; i++) {
+        // Returns '16-bit int' (using ' 32-bit int' to prevent overflow)
+        int16_t dato = ads1115.readADC_SingleEnded(INPUT_PIN_HUMIDITY);
+
+        sum += 100*HUMIDITY_RANGE_LOW/(HUMIDITY_RANGE_LOW-HUMIDITY_RANGE_HIGH)-dato*100/(HUMIDITY_RANGE_LOW-HUMIDITY_RANGE_HIGH);
+
+        delay(10);  //  Wait between readings
+    } // for
+
+    // Get average reading value
+    averageReading = sum / numReadings;
+
+    // Get percentage
+    humidityPercentage = map(averageReading, HUMIDITY_RANGE_LOW,
+                                    HUMIDITY_RANGE_HIGH, 100, 0);
+
+    return averageReading;
+} // readHumidity()
+
+//----------------------------------------------------------------------
+// Print out received data into the Serial Monitor
+//----------------------------------------------------------------------
+void printSensorReading(float measureValue, char StrLiteral[]) {
+    // Avoid negative values
+    if (measureValue < 0) {
+        measureValue = 0;
+    }
+
+    // Print out measure in new range
+    Serial.print(StrLiteral);
+    Serial.print(" percentage: ");
+    Serial.print(measureValue);
+    Serial.println(" %");
+} // printSensorReading()
