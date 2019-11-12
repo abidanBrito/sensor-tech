4ba347f5c0e78700f67732794328a19b84e08336
Abidán Brito <abidan.brito@gmail.com>
2019-11-12 19:10:55 +0100

    Working implementation

--- Sprint2/code/SensorTemperatura/SensorTemperatura.ino
+++ Sprint2/code/SensorTemperatura/SensorTemperatura.ino
@@ -1,75 +1,75 @@
-//-----------------------------------------------------------
-// Autores: Pablo Enguix y Luis Belloch
-// Fecha: 29/10/2019
-//-----------------------------------------------------------
+/*  ------------------------------------------------------------------------
+    AUTHOR:        Abidan Brito, Elvira Montagud, Luis Belloch, Pablo Enguix
+    FILE:          mainProgram.ino
+    DATE:          29/10/2019
+    STATE:         DONE
+    FUNCTIONALITY: This is the main file that is to be executed. It can be
+                   thought of as the center of the codebase.
+    ------------------------------------------------------------------------
+    NOTICE: (C) Copyright 2019. All rights reserved.
+    ------------------------------------------------------------------------ */
 
-//
-//Librerías del Ads
-//
+//// IMPORT EXTERNAL LIBRARIES ////
 #include <Wire.h>
 #include <Adafruit_ADS1015.h>
 
+// Create an ADC object
 Adafruit_ADS1115 ads1115(0x48);
 
-#define power_pin 13
+//// MACRO DEFINITIONS ////
+#define POWER_PIN       13      // Power pin NTC sensor
 
 void setup() {
+    // Set data rate for serial data transmission
+    Serial.begin(9600);
 
-  //
-  // Se prepara el pin de alimentación
-  //
-  pinMode (power_pin, OUTPUT);
+    // Turn on ADS1115
+    ads1115.begin();
 
-  //
-  // Se prepara el serial plot
-  //
-  Serial.begin(9600);
+    // Gain (ADC Range) -> +/- 4.096 V (1 bit = 2 mV)
+    ads1115.setGain(GAIN_ONE);
+} // setup()
 
-  //
-  // Se enciende el ads y se pone a gain one: +/- 4.096V (1 bit=2mV)
-  //
-  ads1115.begin();
+float readVoltageTemperature(int numReadings, int outputPin){
+    float averageReading = 0.0;
+    int16_t sum = 0;
 
-  ads1115.setGain(GAIN_ONE);
+    for(int i = 0; i < numReadings; i++){
+        sum += ads1115.readADC_SingleEnded(outputPin);
+        delay(10);
+    }
 
-}
+    // Get average reading value
+    averageReading = (sum / numReadings);
 
-//-----------------------------------------------------------
-// Z, Z, Z, Z -> medirTemperatura -> R
-//-----------------------------------------------------------
-float medirTemperatura(int cantidadMedidas, int limiteBajo, int limiteAlto, int ADC){
+    // Convert it to (0 - 4.096 V) range
+    averageReading = (float) map(averageReading, 0, 32768, 0, 4096) / 1000;
 
-  float medida = 0.0;
-  int16_t sum = 0;
-  float porcentaje = 0.0;
-
-  digitalWrite(power_pin, HIGH);
-
-  //
-  // Bucle de medidas y media artimética
-  //
-  for(int i = 0; i < cantidadMedidas; i++){
-
-  sum += ads1115.readADC_SingleEnded(ADC);
-    
-  }
-
-  medida = sum/cantidadMedidas;
-  
-  digitalWrite(power_pin, LOW);
-
-  porcentaje = map(medida, limiteBajo, limiteAlto, 25000, 40000)/1000.00;
-
-  return porcentaje;
-  
-}
+    return averageReading;
+} // readTemperature()
 
 void loop() {
+    float voltageReading = 0.0, temperatureReading = 0.0;
 
-  float medida = medirTemperatura(10, -111, -80, 2);
+    // Find temperature
+    voltageReading = readVoltageTemperature(5, 2);
+    temperatureReading = voltageToTemperature(voltageReading);
 
-  Serial.println(medida);
+    // Print out result
+    Serial.println(voltageReading);         // Volts
+    Serial.println(temperatureReading);     // Degrees Celsius
+    delay(1000);
+} // loop()
 
-  delay(1000);
+float voltageToTemperature(float voltage) {
+    float temperature;
+    float y_Intercept = 0.786;
+    float slope = 0.0348;
+    float d_Temp = -0.24;
 
-}
+    // Apply formula
+    temperature = ((voltage - y_Intercept) / slope) + d_Temp;
+
+    return temperature;
+} // voltageToTemperature()
+
